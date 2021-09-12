68f8646daa608d78121d4b1f61c389988703d624
Sofia-Ortega <s.ortega@tamu.edu>
2021-09-12 11:27:27 -0500

    added buttons for play and pause

--- src/pages/SongPage/SongDetails.js
+++ src/pages/SongPage/SongDetails.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import IconButton from '@material-ui/core/IconButton';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import PauseIcon from '@material-ui/icons/Pause';
+import Badge from '@material-ui/core/Badge';
+import NotificationsIcon from '@material-ui/icons/Notifications';
 
 export default function SongDetails({noteDat}) {
   return(
 
     <div>
       <div>
-        Recording icons
+        <IconButton aria-label="show 4 new mails" color="inherit">
+            <PlayArrowIcon />
+        </IconButton>
+        <IconButton aria-label="show 17 new notifications" color="inherit">
+            <PauseIcon />
+        </IconButton>
       </div>
       <div>
         <h6>Notes:</h6>
